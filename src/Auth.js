55335eb6d0d0fd290958ebbdab60dbb5259e2b20
uaqil <uaqil@innovaj.com>
2023-09-14 13:54:43 +0100

    2nd commit

--- src/Auth.js
+++ src/Auth.js
@@ -1,35 +1,66 @@
-import { Box, Button, TextField, Typography } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react';
+import { Box, Button, TextField, Typography } from '@mui/material';
+import axios from 'axios';
+
 
 const Auth = () => {
-  return (
-	<div>
-	  <form>
-		<Box
-		 display='flex' 
-		 flexDirection={'column'} 
-		 maxWidth={400} 
-		 alignItems='center'
-		 justifyContent={'center'}
-		 margin='auto'
-		 marginTop={5}
-		 padding={3}
-		 borderRadius={5}
-		 boxShadow={'5px 5px 10px #ccc'}
-		 sx={{':hover': {
-			boxShadow:'10px 10px 20px #ccc',
-		 } }}>
-			<Typography variant='h2' padding={3} textAlign='center'>
-				Login abc
-			</Typography>
-			<TextField margin='normal' type={'text'} variant='outlined' placeholder='Name'></TextField>
-			<TextField margin='normal' type={'email'} variant='outlined' placeholder='Email'></TextField>
-			<TextField margin='normal' type={'password'} variant='outlined' placeholder='Password'></TextField>
-			<Button sx={{marginTop:3, borderRadius: 3}} variant='contained' color='warning'>Login</Button>
-		</Box>
-	  </form>
-	</div>
-  )
-}
+	const [isSignup, setIsSignup] = useState(false);
+  const [inputs, setInputs] = useState({
+    name:'',
+    email:'',
+    password:'',
+  });
+  const handleChange =(e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name] : e.target.value
+    }))
+  };
+  const handleSubmit = async (e) => {
+    // e.preventFefault();
+    const resp = await axios({
+      url: 'https://randomuser.me/api/',
+      method: 'get',
+    })
+    if(resp.status === 200){
+      console.log(resp.data)
+    }
 
-export default Auth
+  }
+  return (
+    <div>
+      <form >
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            maxWidth: 400,
+            alignItems: 'center',
+            justifyContent: 'center',
+            margin: 'auto',
+            marginTop: 5,
+            padding: 3,
+            borderRadius: 5,
+            boxShadow: '5px 5px 10px #ccc',
+            '&:hover': {
+              boxShadow: '10px 10px 20px #ccc',
+            },
+          }}
+        >
+          <Typography variant='h2' padding={3} textAlign='center'>
+            {isSignup ? 'Signup' : 'Signin'}
+          </Typography>
+          {isSignup && (<TextField onChange={handleChange} name='name' value={inputs.name} margin='normal' type='text' variant='outlined' placeholder='Name' />)}
+          <TextField onChange={handleChange} name='email' value={inputs.email}  margin='normal' type='email' variant='outlined' placeholder='Email' />
+          <TextField onChange={handleChange} name='password' value={inputs.password} margin='normal' type='password' variant='outlined' placeholder='Password' />
+          <Button  sx={{ marginTop: 3, borderRadius: 3 }} onClick={handleSubmit} variant='contained' color='warning'>
+            {isSignup ? 'Sign up' : 'Sign in'}
+          </Button>
+          <Button onClick={() => setIsSignup(!isSignup)} sx={{ marginTop: 3, borderRadius: 3 }}>Change to {isSignup ? 'Login' : 'Signup' }</Button>
+        </Box>
+      </form>
+    </div>
+  );
+};
+
+export default Auth;
